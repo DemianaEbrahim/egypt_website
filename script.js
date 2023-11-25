926bfbd022f1a01cae19b536023ed9064b1b6aa3
Your Name <you@example.com>
2023-11-25 13:14:45 +0200

    egypt website

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("section6Button1").onclick=function(){
+    alert("text copied")
+}
+document.getElementById("section6Button2").onclick=function(){
+    alert("text copied")
+}
+document.getElementById("section6Button3").onclick=function(){
+    alert("text copied")
+}
